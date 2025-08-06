942c3432fee1d9e49374b761732d7d55ae2a618e
xMajedz <xmajedz@gmail.com>
2025-08-06 05:04:21 +0300

    More p5js functions

--- src/p5js.h
+++ src/p5js.h
@@ -1,2 +1,14 @@
-void createCanvas(int width, int height);
-void background(int color);
+#include <stdint.h>
+
+void createCanvas(uint32_t width, uint32_t height);
+void background(uint32_t color);
+void noFill();
+void noStroke();
+
+void fill(uint32_t color);
+void fillrgb(uint32_t r, uint32_t g, uint32_t b);
+
+void stroke();
+void circle(uint32_t x, uint32_t y, uint32_t d);
+
+float deltaTime();
--- src/sketch.c
+++ src/sketch.c
@@ -1,12 +1,43 @@
 #include "p5js.h"
 
+uint32_t canvasW, canvasH, posX, posY, radius;
+int32_t velX, velY, gravity;
+
 void setup() 
 {
-	createCanvas(400, 400);
+	canvasW = 1600;
+	canvasH = 900;
+	posX = 0.5*canvasW;
+	posY = 0.5*canvasH;
+	velX = 200;
+	velY = 200;
+	gravity = 1000;
+	radius = 100;
+	createCanvas(canvasW, canvasH);
 }
 
 void draw()
 {
-	background(220);
-}
+	float dt = deltaTime();
 
+	velY += gravity * dt;
+
+	uint32_t dX = posX + velX * dt;
+	if (dX + radius > canvasW || dX < radius) {
+		velX *= -1;
+	} else {
+		posX = dX;
+	}
+
+	uint32_t dY = posY + velY * dt;
+	if (dY + radius > canvasH || dY < radius) {
+		velY *= -1;
+	} else {
+		posY = dY;
+	}
+
+	background(0);
+	noStroke();
+	fillrgb(255, 0, 0);
+	circle(posX, posY, 2*radius);
+}
